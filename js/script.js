6147062ff3e1762fc14ba66fe3f4dbddb0e7dd5b
WestRemote <wolfsignin@gmail.com>
2021-09-14 18:14:18 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ var personParent = {
 // =========Bracket Notation===========
 var shortObject = {name2: "William", age: 31};
 var secondName = "name2";
-console.log(secondName);
-console.log(shortObject.name2);
-delete shortObject.name2;
-console.log(shortObject[secondName]);
+// console.log(secondName);
+// console.log(shortObject.name2);
+// delete shortObject.name2;
+// console.log(shortObject[secondName]);
 
 
 
@@ -30,16 +30,91 @@ var mixedArray = [
   100,
   "Hello",
   {},
-  ["nested array"]
+  ["Nested array", "Second item in nest"]
 ];
 
 shortArray[0] = 1000; // Overwrites the prior value, 1, with 100
 
-console.log(shortArray[0]); // Outputs "1", the first value
-console.log(mixedArray[3] [0]); // Outputs "nested array"
-
+// console.log(shortArray[0]); // Outputs "1", the first value
+// console.log(mixedArray[3] [1]); // Outputs "nested array"
 
 var myArray = [1,2];
 myArray.push(3);
-myArray.push("Test");
+myArray.push("Test", "Five");
 console.log(myArray); // Will output: [1,2,3, "Test"]
+
+
+
+// ==========Functions==========
+function add(number1, number2) {
+  return number1 + number2;
+}
+var result = add(7, "yes");
+console.log(result);
+
+
+var subtract = function (num3, num4) {
+  return num3 - num4;
+}
+var myObject = {};
+myObject.subtract = function(number1, number2) {
+  // ...
+};
+
+console.log(subtract);
+
+
+// =======Variables==========
+var a = [1, 2, 5];
+var b = a; // Both a & b contain the same array instance
+a.push(3, 6); // Both a & b will be [1,2,3]
+// so when we add to a (or b), the other will always stay exactly the same
+console.log(a);
+console.log(b);
+
+var a2 = 1;
+var b2 = a2; // Both a & b now have the value 1
+a2 = 2; // Now, a = 2 and b remains 1
+console.log(a2, b2);
+
+var a3 = { age: 1 };
+var b3 = a3;
+b3.name = "John"; // a.name will now also be "John"
+console.log(a3);
+console.log(b3);
+console.log(age);
+
+
+
+// =======Conditionals========
+age = 20;
+if (age === 20) {
+  console.log("You are 20 years old!");
+} else {
+  // console.log("You are not 20 years old...");
+}
+
+
+// =======Comparisons===========
+var a4 = [1, 2];
+var b4 = a4; // b is now sharing the same array instance as a.
+// Remember, adding a value to a would also add it to b
+var c4 = [1, 2]; // c has the same content as a & b, but is a different instance.
+// Changing it has no effect on a & b.
+
+// a4 === b4; // true
+// a4 === c4; // false
+// b4 == c4; //false - the non-strict comparator also returns false here
+
+if (a4 === c4) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+console.log("Fields 1 through 4");
+var field1 = true || false; // will be true
+var field2 = false || true; // will also be true
+var field3 = false || "hello"; // will be "hello"
+var field4 = 100 || "hello"; // will be 100
+console.log(field1, field2, field3, field4);
